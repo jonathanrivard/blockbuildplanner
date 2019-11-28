0e8244b60a00308f771f009d3a8700878156d0b4
John Rivard <jrivard23@outlook.com>
2019-11-28 17:41:20 -0600

    Drag and Drop Square

--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@ console.log("Javascript File Loaded");
 
 var items = new Array();
 var container = document.getElementById("item-container");
-container.onmousedown = toggleColor;
+var xDiff, yDiff;
+container.onmousedown = function(){
+    xDiff = parseFloat(getComputedStyle(container).left) - parseFloat(event.pageX);
+    yDiff = parseFloat(getComputedStyle(container).top) - parseFloat(event.pageY);
+    console.log("X: " + xDiff + "\tY: " + yDiff);
+    document.onmousemove = attachContainer;
+};
+container.onmouseup = function(){document.onmousemove = null;};
+
+
+
+function attachContainer(event){
+    var mouseX = event.pageX;
+    var mouseY = event.pageY;
+    container.style.left = (mouseX + xDiff).toString() + "px";
+    container.style.top = (mouseY + yDiff).toString() + "px";
+}
+
+function printMouseChoord(event){
+    console.log(event.pageX + " " + event.pageY);
+}
 
 function toggleColor(){
     console.log("Clicked");
@@ -13,4 +33,8 @@ function toggleColor(){
     }else {
         container.style.backgroundColor = "rgb(0, 0, 0)";
     }
+
+    var current = parseFloat(getComputedStyle(container).left.split("px")[0]);
+    current += 10;
+    container.style.left = current.toString() + "px";    
 }
