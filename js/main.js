e93a8cf26eb2efbc9976b4b28a110b02f6870b87
JohnPC <johnrivard23@gmail.com>
2019-11-28 21:51:04 -0600

    Clean up

--- js/main.js
+++ js/main.js
@@ -1,45 +1,8 @@
 console.log("Javascript File Loaded");
 var items = new Array();
 var container = document.getElementById("item-container");
-//var xDiff, yDiff;
+var test = document.getElementById("test");
 
-var dragContainer = new DragAndDrop(container);
-
-/*
-container.onmousedown = function(){
-    xDiff = parseFloat(getComputedStyle(container).left) - parseFloat(event.pageX);
-    yDiff = parseFloat(getComputedStyle(container).top) - parseFloat(event.pageY);
-    console.log("X: " + xDiff + "\tY: " + yDiff);
-    document.onmousemove = attachContainer;
-};
-container.onmouseup = function(){document.onmousemove = null;};
-
-
-
-function attachContainer(event){
-    var mouseX = event.pageX;
-    var mouseY = event.pageY;
-    container.style.left = (mouseX + xDiff).toString() + "px";
-    container.style.top = (mouseY + yDiff).toString() + "px";
-}
-
-*/
-
-function printMouseChoord(event){
-    console.log(event.pageX + " " + event.pageY);
-}
-
-function toggleColor(){
-    console.log("Clicked");
-    if(getComputedStyle(container).backgroundColor == "rgb(0, 0, 0)"){
-        container.style.backgroundColor = "rgb(255, 0, 0)";
-    }else if(getComputedStyle(container).backgroundColor == "rgb(255, 0, 0)"){
-        container.style.backgroundColor = "rgb(0, 0, 0)";
-    }else {
-        container.style.backgroundColor = "rgb(0, 0, 0)";
-    }
-
-    var current = parseFloat(getComputedStyle(container).left.split("px")[0]);
-    current += 10;
-    container.style.left = current.toString() + "px";    
-}
+//Make divs dragable
+new DragAndDrop(container);
+new DragAndDrop(test);
